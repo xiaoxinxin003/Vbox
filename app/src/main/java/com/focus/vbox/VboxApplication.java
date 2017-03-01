c775c4f5474c1d1f95f876379e7db65e56813357
yang <yangxinxiao@123juzi.com>
2017-03-01 19:24:06 +0800

    [master]新建类，application中进行app的一些初始化操作。

---
+++ app/src/main/java/com/focus/vbox/VboxApplication.java
@@ -0,0 +1,11 @@
+package com.focus.vbox;
+
+import android.app.Application;
+
+/**
+ * Created by focus on 17-3-1.
+ */
+
+public class VboxApplication extends Application {
+
+}
---
+++ app/src/main/java/com/focus/vbox/utils/FileUtils.java
@@ -0,0 +1,9 @@
+package com.focus.vbox.utils;
+
+/**
+ * Created by focus on 17-3-1.
+ */
+
+public class FileUtils {
+
+}
