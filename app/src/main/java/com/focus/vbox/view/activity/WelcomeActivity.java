b7ed36f2a0eb6ee21c3f53c498bacc9e6a50ad5e
xiaoxinxin003 <xiaoxinxin003@outlook.com>
2017-05-22 21:11:34 +0800

    [master]增加欢迎页面。

---
+++ app/src/main/java/com/focus/vbox/view/activity/WelcomeActivity.java
@@ -0,0 +1,17 @@
+package com.focus.vbox.view.activity;
+
+
+import com.focus.vbox.base.BaseActivity;
+
+/**
+ * Created by focus on 2017/5/20.
+ */
+
+public class WelcomeActivity extends BaseActivity {
+
+
+
+
+
+
+}
